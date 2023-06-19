21861abd1a3a284fcdea0595280fa5ea0bbbbf63
jmbeitia <joaquin.manuel.beitia@gmail.com>
2023-06-19 18:45:30 -0300

    Cambio ruta para ejecutar

--- TP - AMq2/src/train_pipeline.py
+++ TP - AMq2/src/train_pipeline.py
@@ -1,5 +1,5 @@
 import subprocess
 
-subprocess.run(['Python', 'feature_engineering.py'])
+subprocess.run(['Python', './src/feature_engineering.py'])
 
-subprocess.run(['Python', 'train.py'])
+subprocess.run(['Python', './src/train.py'])
